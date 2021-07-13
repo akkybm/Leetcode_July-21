3a861f1ed5db8de31e13c10b06d09467d466105a
AKASH MISHRA <amishra_be18@thapar.edu>
2021-07-13 13:28:45 +0530

    first commit

---
+++ Day13_Find Peak Element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int res=0;
+        if(n==1)
+        {
+            return 0;
+        }
+        if(nums[0]>nums[1])
+        {
+            return 0;
+        }
+        if(nums[n-1]>nums[n-2])
+        {
+            return (n-1);
+        }
+        
+        for(int i=1;i<n-1;i++)
+        {
+            if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
+            {
+                res = i;
+            }
+        }
+        return res;
+    }
+};
